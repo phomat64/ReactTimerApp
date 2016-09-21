2f8e5672fe38fbe42f24fbeeec1d9a8b97915546
Christopher Pham <christopherpham@Christophers-MacBook-Pro.local>
2016-09-21 00:13:21 -0500

    Add CountdownForm component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,11 @@ var Main = React.createClass({
   render: function() {
     return (
       <div>
-        <div>
-          <Navigation/>
-          <p>Main.jsx Rendered</p>
+        <Navigation/>
+        <div className="row">
+          <div className="column small-centered medium-6 large-4">
             {this.props.children}
+          </div>
         </div>
       </div>
     );
